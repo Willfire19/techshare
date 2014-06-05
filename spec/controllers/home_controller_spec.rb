bd04d009f72cc90f2cecab6a0dd67dbb307bd061
Willfire19 <willfire19@gmail.com>
2014-06-05 11:05:20 -0400

    Two passing tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,13 +2,30 @@ require 'rails_helper'
 
 RSpec.describe HomeController, :type => :controller do
 
-	describe "GET home page" do
+	describe "Home:" do
 
-		it "responds successfully" do
-			get :home
-			expect(response).to be_success
-			expect(response.status).to eq(200)
+		subject{ page }
+
+		describe "Getting the home page" do
+			it "responds successfully" do
+				get :home
+				expect(response).to be_success
+				expect(response.status).to eq(200)
+			end
 		end
+
+		describe "Home route", :type => :feature do
+
+			# before { visit root_path }
+
+			it "should be the root path" do
+				# page.source.should have_content('This is the home page')
+				visit root_path
+				# expect(rendered).to match "TechShare"
+				expect(response).to render_template("home")
+			end
+		end
+
 	end
 	
 end
---
+++ spec/requests/home_pages_requests_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "Home" do
+
+	# describe "Home page", :type => :feature do
+
+	# 	it "is the root path" do
+	# 		visit root_path
+
+	# 		should have_selector("h1", text:"This is the home page") 
+	# 	end
+
+	# end
+	
+end
